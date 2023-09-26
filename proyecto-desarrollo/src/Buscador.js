50a98e0415d140d7faa38cbab2d54a53560dbc98
Carlos Daniel Corrales <carlox59681@gmail.com>
2023-09-26 01:14:04 -0500

    agregado el boton de acceder para poder ingresar a la vista del login

--- proyecto-desarrollo/src/Buscador.js
+++ proyecto-desarrollo/src/Buscador.js
@@ -1,11 +1,22 @@
 import './Buscador.css'
+import { Link } from 'react-router-dom'
 import LupaIcon from './lupa.png'
 
 function Buscador(){
 	return(
-		<div className="buscador-container">
-			<input type="search" placeholder="Busca tu carro" className="Buscador"/>
-			<img src={LupaIcon} alt="Lupa" className='lupa-icono'/>
+		<div className="section-container">
+			<div className="search-container">
+				<div className="search-content">
+					<input type="search" placeholder="Busca tu carro" className="Buscador"/>
+					<img src={LupaIcon} alt="Lupa" className='lupa-icono'/>
+				</div>
+			</div>
+
+			<div className="login-container">
+				<div className="login-content">
+					<Link to="./login" className="login-button">Acceder</Link>
+				</div>
+			</div>
 		</div>
 	);
 }
