5d8fe436c3cba269cd891441f76cb35d04943952
Carlos Daniel Corrales <carlox59681@gmail.com>
2023-09-25 21:22:10 -0500

    boton de conoce mas terminado

--- proyecto-desarrollo/src/BotonConoceMas.js
+++ proyecto-desarrollo/src/BotonConoceMas.js
@@ -5,7 +5,7 @@ function BotonConoceMas(){
 	return(
 		<div className='button-container'>
 			<Link to="/carros">
-				<button className='button'>Conoce mas</button>
+				<button className='button'>Conoce mas . . .</button>
 			</Link>
 		</div>
 	);
