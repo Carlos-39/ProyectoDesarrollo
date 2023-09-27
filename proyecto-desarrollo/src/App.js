2bee7c20e2423e951d0f3180a1a2166fcb255042
Carlos Daniel Corrales <carlox59681@gmail.com>
2023-09-26 21:57:41 -0500

    Organizado el App.js para poder cambiar de vista y que se renderice lo que va en cada vista, esta organizado todo en bloques ya

--- proyecto-desarrollo/src/App.js
+++ proyecto-desarrollo/src/App.js
@@ -12,26 +12,70 @@ import { Footer } from './Footer';
 function App() {
   return (
     <Router>
-      {/* <Routes>
-        <Route path='/carros'>
-        </Route>
-      </Routes> */}
-
+      {/* organizacion de toda la pagina web */}
+      {/* Buscador, Header, Footer son componentes que estaran en todas las vistas */}
       <Buscador/>
       <Header/>
-      <Slogan/>
-      <TitleMainView/>
-      <Main/>
-      <BotonConoceMas/>
+
+      {/* rutas para poder cambiar de vistas con sus respectivos renders */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/carros" element={<VistaCarros/>} />
+        <Route path="/sucursales" element={<VistaSucursales/>} />
+        <Route path="/taller" element={<VistaTaller/>} />
+        <Route path="/about-us" element={<VistaAboutUs/>} />
+        <Route path="/login" element={<VistaLogin/>} />
+      </Routes>
+
       <Footer/>
     </Router>
   );
 }
 
-function Prueba(){
+//Renderizado de la vista principal
+function Home(){
   return (
     <div>
-      <h1>Prueba</h1>
+      <Slogan/>
+      <TitleMainView/>
+      <Main/>
+      <BotonConoceMas/>
+    </div>
+  );
+}
+
+//Renderizado de la vista de carros
+function VistaCarros(){
+  return (
+    <div>
+    </div>
+  );
+}
+
+function VistaSucursales(){
+  return (
+    <div>
+    </div>
+  );
+}
+
+function VistaTaller(){
+  return (
+    <div>
+    </div>
+  );
+}
+
+function VistaAboutUs(){
+  return (
+    <div>
+    </div>
+  );
+}
+
+function VistaLogin(){
+  return (
+    <div>
     </div>
   );
 }
